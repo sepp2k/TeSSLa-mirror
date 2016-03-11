5f4a9745c836d642cb2244a0ee3b827443700d62
Jannis Harder <jix@jixco.de>
2016-03-11 13:29:15 +0100

    Generate node graph for typechecker

--- tessla/src/main/scala/de/uni_luebeck/isp/tessla/TypeChecker.scala
+++ tessla/src/main/scala/de/uni_luebeck/isp/tessla/TypeChecker.scala
@@ -1,5 +1,7 @@
 package de.uni_luebeck.isp.tessla
 
+import scala.collection.mutable
+
 object TypeChecker extends CompilerPass[Definitions, FunctionGraph] {
   override def apply(compiler: Compiler, defs: Definitions) = {
     assert(defs.macroDefs.isEmpty, "macros still present during typecheck")
@@ -9,10 +11,12 @@ object TypeChecker extends CompilerPass[Definitions, FunctionGraph] {
     ???
   }
 
+  case class Node(result: TypeVar, fn: ExprTreeFn, args: Map[ArgName, TypeVar], subtree: ExprTree)
 
 }
 
 class TypeChecker(compiler: Compiler, defs: Definitions) {
+  import TypeChecker._
   /*
   Plan:
 
@@ -28,4 +32,46 @@ class TypeChecker(compiler: Compiler, defs: Definitions) {
   (type annotation needed)
 
   */
+
+  val sdefs = defs.streamDefs
+
+  val defTypes: mutable.Map[String, Option[TypeVar]] = mutable.Map()
+  val nodes: mutable.Map[TypeVar, Node] = mutable.Map()
+
+
+  def handleName(fn: NamedFn, expr: ExprTree): TypeVar = {
+    if (sdefs contains fn.name) {
+      handleStreamDef(sdefs(fn.name))
+    } else {
+      val v = new TypeVar
+      nodes(v) = Node(v, fn, Map(), expr)
+      v
+    }
+  }
+
+  def handleStreamDef(sdef: StreamDef): TypeVar = {
+    if (defTypes contains sdef.name) {
+      // TODO This fails on cycles, generate diagnostics
+      defTypes(sdef.name).get
+    } else {
+      defTypes(sdef.name) = None
+      val v = handleSubtree(sdef.expr)
+      defTypes(sdef.name) = Some(v)
+      v
+    }
+  }
+
+  def handleSubtree(expr: ExprTree): TypeVar = {
+    if (expr.args.isEmpty && expr.fn.isInstanceOf[NamedFn]) {
+      handleName(expr.fn.asInstanceOf[NamedFn], expr)
+    } else {
+      val v = new TypeVar
+      nodes(v) = Node(v, expr.fn, expr.args.mapValues(handleSubtree), expr)
+      v
+    }
+  }
+
+  for (sdef <- sdefs.values) {
+    handleStreamDef(sdef)
+  }
 }
