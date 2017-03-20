f878dd9044e1ce1b14a4458879fdd4d28bfb433d
Sebastian Hungerecker <sebastian.hungerecker@isp.uni-luebeck.de>
2017-03-20 23:14:38 +0100

    Remove unnecessary laziness
    In an attempt to fix the infinite recursion problem, I've added
    laziness in as many places as I could think of. Now that the issue is
    fixed, I've removed the laziness again from every place where it
    wasn't necessary (i.e. everywhere where I've added it before).

--- src/main/scala/de/uni_luebeck/isp/tessla/interpreter/Interpreter.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/interpreter/Interpreter.scala
@@ -7,75 +7,73 @@ class Interpreter(val spec: TesslaCore.Specification) extends Specification[BigI
   case class InterpreterError(message: String, loc: NestedLoc) extends RuntimeException(s"Interpreter error at $loc: $message")
 
   sealed abstract class Value
-  final case class StreamValue(stream: Lazy[Stream[Lazy[PrimValue]]]) extends Value
+  final case class StreamValue(stream: Stream[PrimValue]) extends Value
 
   sealed abstract class PrimValue extends Value
-  final case class IntValue(i: Lazy[BigInt]) extends PrimValue
-  final case class BoolValue(b: Lazy[Boolean]) extends PrimValue
+  final case class IntValue(i: BigInt) extends PrimValue
+  final case class BoolValue(b: Boolean) extends PrimValue
   final case object UnitValue extends PrimValue
 
-  val inStreams: Map[String, Input[Lazy[PrimValue]]] = spec.inStreams.map {
+  val inStreams: Map[String, Input[PrimValue]] = spec.inStreams.map {
     case (name, _) =>
-      (name, Input[Lazy[PrimValue]]())
+      (name, Input[PrimValue]())
   }.toMap
 
   lazy val defs: Map[String, Lazy[Value]] = spec.streams.map {
     case (name, exp) => (name, Lazy(eval(exp)))
   }
 
-  lazy val outStreams: Map[String, Stream[Lazy[PrimValue]]] = spec.outStreams.map {
+  lazy val outStreams: Map[String, Stream[PrimValue]] = spec.outStreams.map {
     case (name, _) => defs(name).get match {
-      case StreamValue(s) => name -> s.get
-      case value: PrimValue => name -> nil.default(Lazy(value))
+      case StreamValue(s) => name -> s
+      case value: PrimValue => name -> nil.default(value)
     }
   }.toMap
 
-  private def liftBinIntOp(opName: String, op: (=>BigInt, =>BigInt) => Lazy[BigInt], lhs: =>PrimValue, lhsLoc: NestedLoc, rhs: =>PrimValue, rhsLoc: NestedLoc): Lazy[PrimValue] =
-    Lazy {
-      (lhs, rhs) match {
-        case (IntValue(l), IntValue(r)) =>
-          IntValue(Lazy(op(l, r)))
-        case (IntValue(_), r) =>
-          throw InterpreterError(s"Invalid type for right operand of $opName: ${r.getClass.getSimpleName}", rhsLoc)
-        case (l, _) =>
-          throw InterpreterError(s"Invalid type for left operand of $opName: ${l.getClass.getSimpleName}", lhsLoc)
-      }
+  private def liftBinIntOp(opName: String, op: (BigInt, BigInt) => BigInt, lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue =
+    (lhs, rhs) match {
+      case (IntValue(l), IntValue(r)) =>
+        IntValue(op(l, r))
+      case (IntValue(_), r) =>
+        throw InterpreterError(s"Invalid type for right operand of $opName: ${r.getClass.getSimpleName}", rhsLoc)
+      case (l, _) =>
+        throw InterpreterError(s"Invalid type for left operand of $opName: ${l.getClass.getSimpleName}", lhsLoc)
     }
 
-  private def add(lhs: =>PrimValue, lhsLoc: NestedLoc, rhs: =>PrimValue, rhsLoc: NestedLoc): Lazy[PrimValue] = {
-    liftBinIntOp("+", (a,b) => Lazy(a + b), lhs, lhsLoc, rhs, rhsLoc)
+  private def add(lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue = {
+    liftBinIntOp("+", _ + _, lhs, lhsLoc, rhs, rhsLoc)
   }
 
-  private def sub(lhs: =>PrimValue, lhsLoc: NestedLoc, rhs: =>PrimValue, rhsLoc: NestedLoc): Lazy[PrimValue] = {
-    liftBinIntOp("-", (a,b) => Lazy(a - b), lhs, lhsLoc, rhs, rhsLoc)
+  private def sub(lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue = {
+    liftBinIntOp("-", _ - _, lhs, lhsLoc, rhs, rhsLoc)
   }
 
-  private def evalBinOp(op: (=>PrimValue, NestedLoc, =>PrimValue, NestedLoc) => Lazy[PrimValue], lhs: TesslaCore.Expression, rhs: TesslaCore.Expression): Value = {
+  private def evalBinOp(op: (PrimValue, NestedLoc, PrimValue, NestedLoc) => PrimValue, lhs: TesslaCore.Expression, rhs: TesslaCore.Expression): Value = {
     lazy val s1 = getStream(eval(lhs), lhs.loc)
     lazy val s2 = getStream(eval(rhs), rhs.loc)
     lazy val stream = lift(s1 :: s2 :: HNil) {
-      (args: Lazy[PrimValue] :: Lazy[PrimValue] :: HNil) =>
+      (args: PrimValue :: PrimValue :: HNil) =>
         args match {
           case l :: r :: HNil =>
             Some(op(l, lhs.loc, r, rhs.loc))
         }
     }
-    StreamValue(Lazy(stream))
+    StreamValue(stream)
   }
 
-  private def default(values: Value, defaultValue: Value, loc: NestedLoc): Stream[Lazy[PrimValue]] = {
+  private def default(values: Value, defaultValue: Value, loc: NestedLoc): Stream[PrimValue] = {
     (values, defaultValue) match {
       case (StreamValue(v), StreamValue(d)) =>
-        v.get.default(d)
+        v.default(d)
       case (StreamValue(v), d: PrimValue) =>
-        v.get.default(Lazy(d))
+        v.default(Lazy(d))
       case (v, d) =>
         throw InterpreterError(s"Invalid arguments for default: $v, $d", loc)
     }
   }
 
   private def getStream(value: Value, loc: NestedLoc) = value match {
-    case StreamValue(s) => s.get
+    case StreamValue(s) => s
     case other => throw InterpreterError(s"Expected stream, got $other", loc)
   }
 
@@ -84,7 +82,7 @@ class Interpreter(val spec: TesslaCore.Specification) extends Specification[BigI
       defs.getOrElse(name, throw InterpreterError(s"Couldn't find stream named $name", loc)).get
 
     case TesslaCore.Input(name, loc) =>
-      StreamValue(Lazy(inStreams.getOrElse(name, throw InterpreterError(s"Couldn't find stream named $name", loc))))
+      StreamValue(inStreams.getOrElse(name, throw InterpreterError(s"Couldn't find stream named $name", loc)))
 
     case TesslaCore.Add(lhs, rhs, _) =>
       evalBinOp(add, lhs, rhs)
@@ -103,7 +101,7 @@ class Interpreter(val spec: TesslaCore.Specification) extends Specification[BigI
       StreamValue(Lazy(last(getStream(eval(clock), loc), getStream(eval(values), loc))))
 
     case TesslaCore.Nil(loc) =>
-      StreamValue(Lazy(nil))
+      StreamValue(nil)
   }
 }
 
