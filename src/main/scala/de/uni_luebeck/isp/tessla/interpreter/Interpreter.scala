ef04d3826d148ae2087bc49dd578ff0256878c60
Sebastian Hungerecker <sebastian.hungerecker@isp.uni-luebeck.de>
2017-03-24 00:27:48 +0100

    Add missing operations
    Implement the operations that have been added to the Tessla frontend

--- src/main/scala/de/uni_luebeck/isp/tessla/interpreter/Interpreter.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/interpreter/Interpreter.scala
@@ -57,6 +57,62 @@ class Interpreter(val spec: TesslaCore.Specification) extends Specification[BigI
     liftBinIntOp("-", _ - _, lhs, lhsLoc, rhs, rhsLoc)
   }
 
+  private def mul(lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue = {
+    liftBinIntOp("*", _ * _, lhs, lhsLoc, rhs, rhsLoc)
+  }
+
+  private def liftBinBoolOp(opName: String, op: (Boolean, Boolean) => Boolean, lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue =
+    (lhs, rhs) match {
+      case (BoolValue(l), BoolValue(r)) =>
+        BoolValue(op(l, r))
+      case (IntValue(_), r) =>
+        throw InterpreterError(s"Invalid type for right operand of $opName: ${r.getClass.getSimpleName}", rhsLoc)
+      case (l, _) =>
+        throw InterpreterError(s"Invalid type for left operand of $opName: ${l.getClass.getSimpleName}", lhsLoc)
+    }
+
+  private def and(lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue = {
+    liftBinBoolOp("&&", _ && _, lhs, lhsLoc, rhs, rhsLoc)
+  }
+
+  private def or(lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue = {
+    liftBinBoolOp("||", _ || _, lhs, lhsLoc, rhs, rhsLoc)
+  }
+
+  private def liftBinCompOp(opName: String, op: (BigInt, BigInt) => Boolean, lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue =
+    (lhs, rhs) match {
+      case (IntValue(l), IntValue(r)) =>
+        BoolValue(op(l, r))
+      case (IntValue(_), r) =>
+        throw InterpreterError(s"Invalid type for right operand of $opName: ${r.getClass.getSimpleName}", rhsLoc)
+      case (l, _) =>
+        throw InterpreterError(s"Invalid type for left operand of $opName: ${l.getClass.getSimpleName}", lhsLoc)
+    }
+
+  private def lt(lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue = {
+    liftBinCompOp("<", _ < _, lhs, lhsLoc, rhs, rhsLoc)
+  }
+
+  private def gt(lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue = {
+    liftBinCompOp(">", _ > _, lhs, lhsLoc, rhs, rhsLoc)
+  }
+
+  private def lte(lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue = {
+    liftBinCompOp("<=", _ <= _, lhs, lhsLoc, rhs, rhsLoc)
+  }
+
+  private def gte(lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue = {
+    liftBinCompOp(">=", _ >= _, lhs, lhsLoc, rhs, rhsLoc)
+  }
+
+  private def eq(lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue = {
+    liftBinCompOp("==", _ == _, lhs, lhsLoc, rhs, rhsLoc)
+  }
+
+  private def neq(lhs: PrimValue, lhsLoc: NestedLoc, rhs: PrimValue, rhsLoc: NestedLoc): PrimValue = {
+    liftBinCompOp("!=", _ != _, lhs, lhsLoc, rhs, rhsLoc)
+  }
+
   private def evalBinOp(op: (PrimValue, NestedLoc, PrimValue, NestedLoc) => PrimValue, lhs: TesslaCore.Expression, rhs: TesslaCore.Expression): Value = {
     lazy val s1 = getStream(eval(lhs), lhs.loc)
     lazy val s2 = getStream(eval(rhs), rhs.loc)
@@ -70,17 +126,6 @@ class Interpreter(val spec: TesslaCore.Specification) extends Specification[BigI
     StreamValue(stream)
   }
 
-  private def default(values: Value, defaultValue: Value, loc: NestedLoc): Stream[PrimValue] = {
-    (values, defaultValue) match {
-      case (StreamValue(v), StreamValue(d)) =>
-        v.default(d)
-      case (StreamValue(v), d: PrimValue) =>
-        v.default(Lazy(d))
-      case (v, d) =>
-        throw InterpreterError(s"Invalid arguments for default: $v, $d", loc)
-    }
-  }
-
   private def getStream(value: Value, loc: NestedLoc) = value match {
     case StreamValue(s) => s
     case other => throw InterpreterError(s"Expected stream, got $other", loc)
@@ -89,28 +134,92 @@ class Interpreter(val spec: TesslaCore.Specification) extends Specification[BigI
   private def eval(exp: TesslaCore.Expression): Value = exp match {
     case TesslaCore.Var(name, loc) =>
       defs.getOrElse(name, throw InterpreterError(s"Couldn't find stream named $name", loc)).get
-
     case TesslaCore.Input(name, loc) =>
       StreamValue(inStreams.getOrElse(name, throw InterpreterError(s"Couldn't find stream named $name", loc)))
-
     case TesslaCore.Add(lhs, rhs, _) =>
       evalBinOp(add, lhs, rhs)
-
     case TesslaCore.Sub(lhs, rhs, _) =>
       evalBinOp(sub, lhs, rhs)
-
-    case TesslaCore.BoolLiteral(b, _) => BoolValue(Lazy(b))
-    case TesslaCore.IntLiteral(i, _) => IntValue(Lazy(i))
+    case TesslaCore.Mul(lhs, rhs, _) =>
+      evalBinOp(mul, lhs, rhs)
+    case TesslaCore.And(lhs, rhs, _) =>
+      evalBinOp(and, lhs, rhs)
+    case TesslaCore.Or(lhs, rhs, _) =>
+      evalBinOp(or, lhs, rhs)
+    case TesslaCore.Not(arg, loc) =>
+      StreamValue(boolStreamToValueStream(!boolStream(getStream(eval(arg), loc), loc)))
+    case TesslaCore.Lt(lhs, rhs, _) =>
+      evalBinOp(lt, lhs, rhs)
+    case TesslaCore.Lte(lhs, rhs, _) =>
+      evalBinOp(lte, lhs, rhs)
+    case TesslaCore.Gt(lhs, rhs, _) =>
+      evalBinOp(gt, lhs, rhs)
+    case TesslaCore.Gte(lhs, rhs, _) =>
+      evalBinOp(gte, lhs, rhs)
+    case TesslaCore.Eq(lhs, rhs, _) =>
+      evalBinOp(eq, lhs, rhs)
+    case TesslaCore.Neq(lhs, rhs, _) =>
+      evalBinOp(neq, lhs, rhs)
+    case TesslaCore.IfThenElse(cond, thenCase, elseCase, loc) =>
+      StreamValue(boolStream(getStream(eval(cond), loc), loc).
+        ifThenElse(getStream(eval(thenCase), loc), getStream(eval(elseCase), loc)))
+    case TesslaCore.IfThen(cond, thenCase, loc) =>
+      StreamValue(boolStream(getStream(eval(cond), loc), loc).
+        ifThen(getStream(eval(thenCase), loc)))
+    case TesslaCore.BoolLiteral(b, _) => BoolValue(b)
+    case TesslaCore.IntLiteral(i, _) => IntValue(i)
     case TesslaCore.Unit(_) => UnitValue
-
     case TesslaCore.Default(values, defaultValue, loc) =>
-      StreamValue(Lazy(default(eval(values), eval(defaultValue), loc)))
-
+      StreamValue(getStream(eval(values), loc).default(eval(defaultValue).asInstanceOf[PrimValue]))
+    case TesslaCore.DefaultFrom(values, defaults, loc) =>
+      StreamValue(getStream(eval(values), loc).default(getStream(eval(defaults), loc)))
     case TesslaCore.Last(values, clock, loc) =>
-      StreamValue(Lazy(last(getStream(eval(clock), loc), getStream(eval(values), loc))))
-
+      StreamValue(last(getStream(eval(clock), loc), getStream(eval(values), loc)))
+    case TesslaCore.DelayedLast(values, delays, loc) =>
+      StreamValue(delayedLast(intStream(getStream(eval(delays), loc), loc), getStream(eval(values), loc)))
     case TesslaCore.Nil(loc) =>
       StreamValue(nil)
+    case TesslaCore.Time(values, loc) =>
+      StreamValue(intStreamToValueStream(getStream(eval(values), loc).time()))
+  }
+
+  def intStream(stream: Stream[PrimValue], loc: NestedLoc): Stream[BigInt] = {
+    lift(stream :: HNil) {
+      (args: PrimValue :: HNil) =>
+        args match {
+          case IntValue(i) :: HNil => Some(i)
+          case value :: HNil => throw InterpreterError(s"Expected it integer value, got: $value", loc)
+        }
+    }
+  }
+
+  def boolStream(stream: Stream[PrimValue], loc: NestedLoc): Stream[Boolean] = {
+    lift(stream :: HNil) {
+      (args: PrimValue :: HNil) =>
+        args match {
+          case BoolValue(b) :: HNil => Some(b)
+          case value :: HNil => throw InterpreterError(s"Expected it integer value, got: $value", loc)
+        }
+    }
+  }
+
+
+  def intStreamToValueStream(stream: Stream[BigInt]): Stream[PrimValue] = {
+    lift(stream :: HNil) {
+      (args: BigInt :: HNil) =>
+        args match {
+          case i :: HNil => Some(IntValue(i))
+        }
+    }
+  }
+
+  def boolStreamToValueStream(stream: Stream[Boolean]): Stream[PrimValue] = {
+    lift(stream :: HNil) {
+      (args: Boolean :: HNil) =>
+        args match {
+          case b :: HNil => Some(BoolValue(b))
+        }
+    }
   }
 }
 
