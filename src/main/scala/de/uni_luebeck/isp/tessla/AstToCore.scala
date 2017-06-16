bbda6ac34cf958c3c20baf152e2186fa9937e2fa
Sebastian Hungerecker <sebastian.hungerecker@isp.uni-luebeck.de>
2017-06-16 18:56:54 +0200

    Implement macros

--- src/main/scala/de/uni_luebeck/isp/tessla/AstToCore.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/AstToCore.scala
@@ -5,7 +5,7 @@ import AstToCore._
 
 class AstToCore(spec: Ast.Spec) {
   var counter = 0
-  val visited = mutable.Map[String, TesslaCore.Arg]()
+  val alreadyTranslated = mutable.Map[String, TesslaCore.Arg]()
 
   val inStreams = spec.statements.collect {
     case Ast.In(name, _, loc) => (name.name, loc)
@@ -27,7 +27,16 @@ class AstToCore(spec: Ast.Spec) {
   type TranslatedExpression = (Seq[(String, TesslaCore.Expression)], TesslaCore.Arg)
   sealed abstract class EnvEntry
   case class BuiltIn(apply: (Seq[TesslaCore.Arg], String, Location) => TranslatedExpression) extends EnvEntry
-  case class Definition(definition: Ast.Def) extends EnvEntry
+  class Definition(val definition: Ast.Def, _closure: =>Env) extends EnvEntry {
+    lazy val closure = _closure
+  }
+
+  object Definition {
+    def apply(definition: Ast.Def, _closure: => Env) = new Definition(definition, _closure)
+    def unapply(d: Definition): Option[(Ast.Def, Env)] = Some((d.definition, d.closure))
+  }
+
+  type Env = Map[(String, Int), EnvEntry]
 
   val builtins: Map[(String, Int), (Seq[TesslaCore.Arg], String, Location) => TranslatedExpression] = Map(
     ("nil", 0) -> {
@@ -74,10 +83,10 @@ class AstToCore(spec: Ast.Spec) {
     }
   )
 
-  val globalEnv: Map[(String, Int), EnvEntry] = builtins.map {
+  lazy val globalEnv: Env = builtins.map {
     case (key, b) => key -> BuiltIn(b)
   } ++ globalDefs.map {
-    case (name, d) => (name, d.macroArgs.length) -> Definition(d)
+    case (name, d) => (name, d.macroArgs.length) -> Definition(d, globalEnv)
   }
 
   def translateSpec() = {
@@ -89,7 +98,7 @@ class AstToCore(spec: Ast.Spec) {
     def outs = outStreams.map { out =>
       val name = out.name.name
       val loc = out.name.loc
-      visited.get(name) match {
+      alreadyTranslated.get(name) match {
         case Some(s: TesslaCore.StreamRef) =>
           s
         case Some(_) =>
@@ -102,14 +111,12 @@ class AstToCore(spec: Ast.Spec) {
     TesslaCore.Specification(streams, inStreams.toSeq, outs)
   }
 
-  def macroExpand(app: Ast.ExprApp) = ???
-
-  def translateExpression(expr: Ast.Expr, name: String, env: Map[(String, Int), EnvEntry]): TranslatedExpression = {
-    if (visited.contains(name)) (Seq(), visited(name))
+  def translateExpression(expr: Ast.Expr, name: String, env: Env): TranslatedExpression = {
+    if (alreadyTranslated.contains(name)) (Seq(), alreadyTranslated(name))
     else {
       // This value will be overridden later. This one will only be reached in case of recursion, in which case
       // it should be the correct one.
-      visited(name) = TesslaCore.Stream(name, UnknownLoc)
+      alreadyTranslated(name) = TesslaCore.Stream(name, UnknownLoc)
       val (defs, arg) = expr match {
         case Ast.ExprBoolLit(Ast.BoolLit(value, loc)) =>
           (Seq(), TesslaCore.BoolLiteral(value, loc))
@@ -121,24 +128,42 @@ class AstToCore(spec: Ast.Spec) {
           (Seq(), TesslaCore.StringLiteral(str, loc))
         case Ast.ExprName(id) =>
           env.get((id.name, 0)) match {
-            case Some(Definition(d)) =>
-              translateExpression(d.definition, id.name, env)
+            case Some(Definition(d, closure)) =>
+              translateExpression(d.definition, id.name, closure)
             case Some(BuiltIn(b)) =>
               b(Seq(), name, id.loc)
             case None =>
               inStreams.get(id.name) match {
                 case Some(loc) => (Seq(), TesslaCore.InputStream(id.name, loc))
-                case None => throw new UndefinedVariable(id)
+                case None => throw UndefinedVariable(id)
               }
           }
-        case Ast.ExprTypeAscr(e, _) => translateExpression(e, name, env)
-        case app @ Ast.ExprApp(id, args, loc) =>
+        case Ast.ExprTypeAscr(e, _) =>
+          alreadyTranslated.remove(name)
+          translateExpression(e, name, env)
+        case Ast.ExprApp(id, args, loc) =>
           env.get((id.name, args.length)) match {
-            case Some(Definition(_)) => translateExpression(macroExpand(app), name, env)
+            case Some(Definition(d, closure)) =>
+              val namedArgs: Env = args.collect {
+                case Ast.NamedArg(argName, expr) =>
+                  if (d.macroArgs.exists(_.name.name == argName.name)) {
+                    val newName = Ast.Identifier(mkId(argName.name), argName.loc)
+                    (argName.name, 0) -> Definition(Ast.Def(newName, Seq(), None, expr, argName.loc), env)
+                  } else throw UndefinedNamedArg(argName)
+              }.toMap
+              val posArgs: Env = d.macroArgs.filterNot { argName =>
+                namedArgs.contains((argName.name.name, 0))
+              }.zip(args.collect { case Ast.PosArg(expr) => expr}).map {
+                case (argName, expr) =>
+                  val newName = Ast.Identifier(mkId(argName.name.name), argName.name.loc)
+                  (argName.name.name, 0) -> Definition(Ast.Def(newName, Seq(), None, expr, argName.name.loc), env)
+              }.toMap
+              alreadyTranslated.remove(name)
+              translateExpression(d.definition, name, closure ++ posArgs ++ namedArgs)
             case Some(BuiltIn(b)) =>
               val coreArgs = args.map {
                 case Ast.PosArg(expr) => translateExpression(expr, mkId(name), env)
-                case Ast.NamedArg(name, _) => throw UndefinedNamedArg(name)
+                case Ast.NamedArg(argName, _) => throw UndefinedNamedArg(argName)
               }
               val (defs, arg) = b(coreArgs.map(_._2), name, loc)
               (coreArgs.flatMap(_._1) ++ defs, arg)
@@ -146,7 +171,7 @@ class AstToCore(spec: Ast.Spec) {
             case None => throw UndefinedFunction(id, args.length)
           }
       }
-      visited(name) = arg
+      alreadyTranslated(name) = arg
       (defs, arg)
     }
   }
