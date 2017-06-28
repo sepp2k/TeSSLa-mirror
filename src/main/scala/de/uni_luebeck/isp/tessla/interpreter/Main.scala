6b57d3b6a1855a2257d0dcaa237763e0d6691386
Sebastian Hungerecker <sebastian.hungerecker@isp.uni-luebeck.de>
2017-06-28 20:39:08 +0200

    Add various command line flags
    Add the command line flags --verify-only, --no-diagnostics
    and --print-core.

--- src/main/scala/de/uni_luebeck/isp/tessla/interpreter/Main.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/interpreter/Main.scala
@@ -10,21 +10,29 @@ object Main extends SexyOpt {
 
   val tesslaFile = posArg("tessla-file", "The file containing the Tessla specification")
   val traceFile = optionalPosArg("trace-file", "The file containing the trace data used as input for the specification." +
-                                               "If this is not provided, input is read froms stdin")
+                                               " If this is not provided, input is read from stdin")
+  val verifyOnly = flag("verify-only", "Only check the Tessla spec for errors and don't execute it")
+  val noDiagnostics = flag("no-diagnostics", "Don't print error messages and warnings")
+  def diagnostics = !noDiagnostics.value
+  val printCore = flag("print-core", "Print the Tessla Core representation generated from the Tessla specification")
 
   def main(args: Array[String]): Unit = {
     parse(args)
     val traceSource = traceFile.value.map(Source.fromFile).getOrElse(Source.stdin)
     val tesslaSpec = Interpreter.fromFile(tesslaFile.value) match {
       case Success(spec, warnings) =>
-        warnings.foreach(w => println(s"Warning: $w"))
+        if (diagnostics) warnings.foreach(w => println(s"Warning: $w"))
+        if (printCore.value) println(spec.spec)
         spec
       case Failure(errors, warnings) =>
-        println(s"Compilation failed with ${warnings.length} warnings and ${errors.length} errors:")
-        warnings.foreach(w => println(s"Warning: $w"))
-        errors.foreach(e => println(s"Error: $e"))
+        if (diagnostics) {
+          println(s"Compilation failed with ${warnings.length} warnings and ${errors.length} errors:")
+          warnings.foreach(w => println(s"Warning: $w"))
+          errors.foreach(e => println(s"Error: $e"))
+        }
         sys.exit(1)
     }
+    if (verifyOnly.value) return
     tesslaSpec.outStreams.foreach { case (name, stream) => tesslaSpec.printStream(stream, name) }
     Traces.feedInput(tesslaSpec, traceSource)
   }
