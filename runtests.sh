eaea173299bb2a22cff89680a8301ef5de2e3af1
Malte Schmitz <malte@schmitz-sh.de>
2017-08-07 22:19:51 +0200

    Emulate globstar using find

--- runtests.sh
+++ runtests.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-shopt -s globstar
-
 # Run the test cases through the JAR
 # This runs the same test cases as "sbt test", but displays the output and diagnostics instead of
 # verifying them and just displaying a check mark
 # Any options given to this script will be passed along to the interpreter, so you can use, for
 # example, --debug or --print-core to run all test cases with that option.
 
-sbt "set test in assembly := {}" clean assembly
-for testcase in src/test/resources/de/uni_luebeck/isp/tessla/interpreter/tests/**/*.tessla
-do
-    echo $(basename $testcase)
-    inputfile="$(dirname $testcase)/$(basename $testcase .tessla).input"
-    java -jar target/scala-2.12/tessla.interpreter-assembly-*.jar "$@" "$testcase" "$inputfile"
-    echo
-done
+export testPath=src/test/resources/de/uni_luebeck/isp/tessla/interpreter/tests/
+function runTest {
+  testcase=$1
+  shift
+  testcaseName=${testcase#$testPath}
+  echo $testcaseName
+  inputfile="$(dirname $testcase)/$(basename $testcase .tessla).input"
+  java -jar target/scala-2.12/tessla.interpreter-assembly-*.jar "$@" "$testcase" "$inputfile"
+  echo
+}
+export -f runTest
+
+echo sbt "set test in assembly := {}" clean assembly
+command='runTest "{}" '"$@"
+find $testPath -name "*.tessla" -exec bash -c "$command" \;
