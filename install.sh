8bf51eafb94dc9a8c961f0cc61b43850b273a0df
Malte Schmitz <m@mlte.de>
2017-08-29 19:00:58 +0200

    Fix install script after artifact renaming

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd target/scala-2.12/ >/dev/null 2>/dev/null || {
   echo "Run sbt assembly first"
   exit 1
 }
-ls tessla.interpreter* >/dev/null 2>/dev/null || {
+ls tessla*.jar >/dev/null 2>/dev/null || {
   echo "Run sbt assembly first"
   exit 1
 }
@@ -14,7 +14,7 @@ ls tessla.interpreter* >/dev/null 2>/dev/null || {
 set -e # cancel script if anything fails
 
 mkdir -p /usr/local/opt/tessla
-cp `ls tessla*` /usr/local/opt/tessla/tessla.jar
+cp tessla*.jar /usr/local/opt/tessla/tessla.jar
 
 printf '#!/bin/bash\njava -jar /usr/local/opt/tessla/tessla.jar $@\n' > /usr/local/bin/tessla
 chmod +x /usr/local/bin/tessla
