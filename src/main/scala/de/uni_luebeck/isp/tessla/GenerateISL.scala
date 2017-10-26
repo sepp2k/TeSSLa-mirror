82a9607b07c564a569ca4bc332e892b2c063b612
Maximilian Krome <maximilian.krome@student.uni-luebeck.de>
2017-10-26 16:22:22 +0200

    Implement Analyser for Tessla
    Generate ISL from Tessla code

--- src/main/scala/de/uni_luebeck/isp/tessla/GenerateISL.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/GenerateISL.scala
@@ -1,8 +1,5 @@
 package de.uni_luebeck.isp.tessla
 
-import de.uni_luebeck.isp.tessla.Tessla.Expression
-import de.uni_luebeck.isp.tessla.TranslationPhase.{Failure, Success}
-
 object GenerateISL {
   def main(args: Array[String]): Unit = {
     if (args.length != 1) {
@@ -10,41 +7,73 @@ object GenerateISL {
       sys.exit(1)
     }
     val tesslaFile = args(0)
-    TesslaParser.translateSpec(TesslaSource.fromFile(tesslaFile)) match {
-      case Success(ast, _) =>
-        generateForSpec(ast)
-      case Failure(_, _) =>
-        System.err.println("Parsing failed")
-    }
+    val ast = new TesslaParser().translateSpec(TesslaSource.fromFile(tesslaFile))
+    println(generateForSpec(ast))
   }
 
-  def generateForSpec(spec: Tessla.Spec): Unit = {
-    spec.statements.foreach(generateForStatement)
+  def generateForSpec(spec: Tessla.Spec): String = {
+    spec.statements.flatMap(generateForStatement).mkString("\n")
   }
 
-  def generateForStatement(statement: Tessla.Statement): Unit = {
+  def generateForStatement(statement: Tessla.Statement): Seq[String] = {
     statement match {
       case Tessla.Definition(_, _, _, body, _) =>
         generateForExpr(body)
       case Tessla.Out(expr, _, _) =>
         generateForExpr(expr)
       case _ =>
-        // Do nothing
+        Seq()
     }
   }
 
-  def generateForExpr(expr: Tessla.Expression): Unit = {
+  def generateForExpr(expr: Tessla.Expression): Seq[String] = {
     expr match {
       case Tessla.MacroCall(id, args, _) =>
-        generateForMacro(id, args)
+        generateForMacro(id, args) ++ args.flatMap {
+          case Tessla.NamedArgument(_, argExpr) =>
+            generateForExpr(argExpr)
+          case Tessla.PositionalArgument(argExpr) =>
+            generateForExpr(argExpr)
+        }
+      case Tessla.TypeAssertion(exprArg, _) =>
+        generateForExpr(exprArg)
+      case Tessla.Block(defs, exprArg,_) =>
+        generateForExpr(exprArg) ++ defs.flatMap{
+          case Tessla.Definition(_, _, _, body, _) =>
+            generateForExpr(body)
+        }
       case _ =>
-        // loop back
+        Seq()
     }
   }
+
 
   def generateForMacro(id: Tessla.Identifier, args: Seq[Tessla.Argument] ): Seq[String] = {
     id match {
-      case Tessla.Identifier("code_line_exec",_) => List(s"line_reached:${args[0].toString}")
+      case Tessla.Identifier("code_line_exec",_) =>
+        args match {
+          case Seq(Tessla.PositionalArgument(Tessla.IntLiteral(value, _))) =>
+            Seq(s"if [line_reached:${value}] then line_reached fi")
+          case _ =>
+            Seq("line_reached")
+        }
+      case Tessla.Identifier("function_call",_) =>
+        args match {
+          // This wont work because we are already in that function when functions evaluates
+          //case Seq(Tessla.PositionalArgument(Tessla.StringLiteral(value, _))) =>
+          //  Seq(s"if [functions:${value}] then function_call fi")
+          case _ =>
+            Seq("function_call")
+        }
+      case Tessla.Identifier("function_return",_) =>
+        args match {
+          case Seq(Tessla.PositionalArgument(Tessla.IntLiteral(value, _))) =>
+            Seq(s"if &([functions:${value}], [opcodes:ret]) then functions opcodes fi")
+          case _ =>
+            Seq("line_reached")
+        }
+      case _ =>
+        Seq()
     }
   }
 }
