eb637ade4e8de45a517ebe493b30860a0be7efc6
Sebastian Hungerecker <sepp2k@googlemail.com>
2018-02-05 21:19:47 +0100

    Memoize

--- src/main/scala/de/uni_luebeck/isp/tessla/DepthChecker.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/DepthChecker.scala
@@ -1,25 +1,28 @@
 package de.uni_luebeck.isp.tessla
 
+import scala.collection.mutable
+
 object DepthChecker {
   def nestingDepth(spec: TesslaCore.Specification): Int = {
+    val memTable: mutable.Map[String, Int] = mutable.Map()
     (spec.outStreams.map {
       case (_, stream) =>
-        nestingDepth(spec.streams, stream, Set())
+        nestingDepth(spec.streams, stream, memTable)
     } ++ spec.streams.collect {
       case (name, l: TesslaCore.Last) =>
-        nestingDepth(spec.streams, l.values, Set())
+        nestingDepth(spec.streams, l.values, memTable)
       case (name, dl: TesslaCore.DelayedLast) =>
         Math.max(
-          nestingDepth(spec.streams, dl.values, Set()),
-          nestingDepth(spec.streams, dl.delays, Set()))
+          nestingDepth(spec.streams, dl.values, memTable),
+          nestingDepth(spec.streams, dl.delays, memTable))
     }).max
   }
 
-  def nestingDepth(streams: Map[String, TesslaCore.Expression], stream: TesslaCore.StreamRef, visited: Set[String]): Int = {
-    if (visited.contains(stream.name)) return 0
+  def nestingDepth(streams: Map[String, TesslaCore.Expression], stream: TesslaCore.StreamRef, memoized: mutable.Map[String, Int]): Int = {
+    if (memoized.contains(stream.name)) return memoized(stream.name)
 
     def visitChild(child: TesslaCore.StreamRef): Int = {
-      nestingDepth(streams, child, visited + stream.name)
+      nestingDepth(streams, child, memoized)
     }
 
     stream match {
@@ -39,6 +42,7 @@ object DepthChecker {
           case dl: TesslaCore.DelayedLast =>
             0
         }
+        memoized(s.name) = 1 + childDepth
         1 + childDepth
     }
   }
