fd6582b29e8250de4b942da6c57da04ad69c2ed1
Malte Schmitz <m@mlte.de>
2018-03-07 15:34:29 +0100

    Speed up recursive depth computation

--- src/main/scala/de/uni_luebeck/isp/tessla/RecursiveDepthChecker.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/RecursiveDepthChecker.scala
@@ -1,40 +1,56 @@
 package de.uni_luebeck.isp.tessla
 
+import scala.collection.mutable
+
 object RecursiveDepthChecker {
   def nestingDepth(spec: TesslaCore.Specification): Int = {
     spec.streams.collect {
       case (name, l: TesslaCore.Last) =>
-        nestingDepth(spec.streams, l.values, Set(name))
+        nestingDepth(spec.streams, l.values, name)
       case (name, dl: TesslaCore.DelayedLast) =>
         Math.max(
-          nestingDepth(spec.streams, dl.values, Set(name)),
-          nestingDepth(spec.streams, dl.delays, Set(name)))
+          nestingDepth(spec.streams, dl.values, name),
+          nestingDepth(spec.streams, dl.delays, name))
     }.fold(0)(math.max)
   }
 
-  def nestingDepth(streams: Map[String, TesslaCore.Expression], stream: TesslaCore.StreamRef, visited: Set[String]): Int = {
+  def nestingDepth(streams: Map[String, TesslaCore.Expression], stream: TesslaCore.StreamRef, origin: String): Int =
+    nestingDepth(streams, stream, origin, mutable.Map()).getOrElse(0)
+
+  def inc(a: Option[Int]): Option[Int] = a.map(_ + 1)
+
+  def nestingDepth(streams: Map[String, TesslaCore.Expression], stream: TesslaCore.StreamRef, origin: String, memoized: mutable.Map[String, Option[Int]]): Option[Int] = {
+    if (memoized.contains(stream.name)) return memoized(stream.name)
+
     stream match {
-      case _: TesslaCore.Nil | _: TesslaCore.InputStream => 0
+      case _: TesslaCore.Nil | _: TesslaCore.InputStream => None
       case s: TesslaCore.Stream =>
-        if (visited.contains(s.name)) return visited.size
 
-        def visitChild(child: TesslaCore.StreamRef): Int =
-          nestingDepth(streams, child, visited + s.name)
+        def visitChild(child: TesslaCore.StreamRef): Option[Int] =
+          nestingDepth(streams, child, origin, memoized)
 
-        streams(s.name) match {
-          case l: TesslaCore.Lift =>
-            l.args.map(visitChild).max
-          case t: TesslaCore.Time =>
-            visitChild(t.stream)
-          case d: TesslaCore.Default =>
-            visitChild(d.stream)
-          case d: TesslaCore.DefaultFrom =>
-            Math.max(visitChild(d.valueStream), visitChild(d.defaultStream))
-          case l: TesslaCore.Last =>
-            visitChild(l.clock)
-          case dl: TesslaCore.DelayedLast =>
-            0
+        val result = if (s.name == origin) {
+          Some(0)
+        } else {
+          streams(s.name) match {
+            case l: TesslaCore.Lift =>
+              val foo: Seq[Option[Int]] = l.args.map(visitChild)
+              foo.max
+            case t: TesslaCore.Time =>
+              visitChild(t.stream)
+            case d: TesslaCore.Default =>
+              visitChild(d.stream)
+            case d: TesslaCore.DefaultFrom =>
+              Seq(visitChild(d.valueStream), visitChild(d.defaultStream)).max
+            case l: TesslaCore.Last =>
+              visitChild(l.clock)
+            case dl: TesslaCore.DelayedLast =>
+              None
+          }
         }
+
+        memoized(s.name) = inc(result)
+        inc(result)
     }
   }
 }
