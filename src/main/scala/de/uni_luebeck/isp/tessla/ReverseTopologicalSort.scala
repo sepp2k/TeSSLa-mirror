3c0ca127b8cee1493f9a3d840dad390b934a872c
Sebastian Hungerecker <sebastian.hungerecker@isp.uni-luebeck.de>
2018-03-16 16:06:22 +0100

    Fix broken topological sort

--- src/main/scala/de/uni_luebeck/isp/tessla/ReverseTopologicalSort.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/ReverseTopologicalSort.scala
@@ -7,19 +7,24 @@ object ReverseTopologicalSort {
   final case class Sorted[Node](nodes: Iterable[Node]) extends Result[Node]
   final case class Cycles[Node](cycleStarts: Seq[Node]) extends Result[Node]
 
-  def sort[Node](startNodes: Iterable[Node])(getChildren: Node => Iterable[Node]): Result[Node] = {
+  def sort[Node](startNodes: Iterable[Node])(getChildren: Node => Seq[Node]): Result[Node] = {
     val visited = mutable.Set[Node]()
     var cyclesStarts: Seq[Node] = Seq()
-    def go(current: Node): Iterable[Node] = {
-      if (visited.contains(current)) {
+    def go(current: Node, stack: Set[Node]): Seq[Node] = {
+      if (stack.contains(current)) {
         cyclesStarts = current +: cyclesStarts
+        Seq()
+      } else if (visited.contains(current)) {
         Seq()
       } else {
         visited += current
-        getChildren(current).flatMap(go)
+        current +: getChildren(current).flatMap(go(_, stack + current))
       }
     }
-    val nodes = startNodes.flatMap(go)
+    val nodes = startNodes.flatMap { startNode =>
+      if (visited(startNode)) Seq()
+      else go(startNode, Set()).reverse
+    }
     if (cyclesStarts.isEmpty) {
       Sorted(nodes)
     } else {
