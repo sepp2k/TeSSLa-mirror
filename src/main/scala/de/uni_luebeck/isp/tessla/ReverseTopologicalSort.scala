fa9ced3906ec7e39b82cf390b754e44147f1b6ac
Sebastian Hungerecker <sebastian.hungerecker@isp.uni-luebeck.de>
2018-05-05 15:38:35 +0200

    Simplify implementation of topological sort

--- src/main/scala/de/uni_luebeck/isp/tessla/ReverseTopologicalSort.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/ReverseTopologicalSort.scala
@@ -7,32 +7,23 @@ object ReverseTopologicalSort {
   final case class Sorted[Node](nodes: Iterable[Node]) extends Result[Node]
   final case class Cycles[Node](nodesInCycles: Seq[Node]) extends Result[Node]
 
-  sealed abstract class Mark
-  case object Permanent extends Mark
-  case object Temporary extends Mark
-
   def sort[Node](startNodes: Iterable[Node])(getChildren: Node => Seq[Node]): Result[Node] = {
-    val marks = mutable.Map[Node, Mark]()
+    val visited = mutable.Set[Node]()
     val nodesInCycles = mutable.ArrayBuffer[Node]()
 
     var nodes = List[Node]()
 
     def go(current: Node, stack: List[Node]): Unit = {
-      marks.get(current) match {
-        case Some(Temporary) =>
-          nodesInCycles ++= stack.slice(0, stack.indexOf(current) + 1)
-        case Some(Permanent) =>
-          // do nothing
-        case None =>
-          marks(current) = Temporary
+      if (stack.contains(current)) {
+        nodesInCycles ++= stack.slice(0, stack.indexOf(current) + 1)
+      } else if (!visited.contains(current)) {
           getChildren(current).foreach(go(_, current :: stack))
-          marks(current) = Permanent
+          visited += current
           nodes = current :: nodes
       }
     }
     startNodes.foreach { startNode =>
-      if (!marks.contains(startNode))
-        go(startNode, List())
+      go(startNode, List())
     }
     if (nodesInCycles.isEmpty) {
       Sorted(nodes.reverse)
