93e2ece89adccedd13eeb61c6f7cd24b07c7216b
Sebastian Hungerecker <sebastian.hungerecker@isp.uni-luebeck.de>
2018-05-23 22:41:48 +0200

    Adjust the depth checkers to the changes in TesslaCore

--- src/main/scala/de/uni_luebeck/isp/tessla/RecursiveDepthChecker.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/RecursiveDepthChecker.scala
@@ -4,35 +4,35 @@ import scala.collection.mutable
 
 object RecursiveDepthChecker {
   def nestingDepth(spec: TesslaCore.Specification): Int = {
+    val streams = spec.streams.toMap
     spec.streams.collect {
-      case (name, l: TesslaCore.Last) =>
-        nestingDepth(spec.streams, l.values, name)
-      case (name, dl: TesslaCore.DelayedLast) =>
+      case (id, TesslaCore.StreamDefinition(l: TesslaCore.Last, _)) =>
+        nestingDepth(streams, l.values, id)
+      case (id, TesslaCore.StreamDefinition(dl: TesslaCore.DelayedLast, _)) =>
         Math.max(
-          nestingDepth(spec.streams, dl.values, name),
-          nestingDepth(spec.streams, dl.delays, name))
+          nestingDepth(streams, dl.values, id),
+          nestingDepth(streams, dl.delays, id))
     }.fold(0)(math.max)
   }
 
-  def nestingDepth(streams: Map[String, TesslaCore.Expression], stream: TesslaCore.StreamRef, origin: String): Int =
+  def nestingDepth(streams: Map[TesslaCore.Identifier, TesslaCore.StreamDefinition], stream: TesslaCore.StreamRef, origin: TesslaCore.Identifier): Int =
     nestingDepth(streams, stream, origin, mutable.Map()).getOrElse(0)
 
   def inc(a: Option[Int]): Option[Int] = a.map(_ + 1)
 
-  def nestingDepth(streams: Map[String, TesslaCore.Expression], stream: TesslaCore.StreamRef, origin: String, memoized: mutable.Map[String, Option[Int]]): Option[Int] = {
-    if (memoized.contains(stream.name)) return memoized(stream.name)
-
+  def nestingDepth(streams: Map[TesslaCore.Identifier, TesslaCore.StreamDefinition], stream: TesslaCore.StreamRef, origin: TesslaCore.Identifier, memoized: mutable.Map[TesslaCore.Identifier, Option[Int]]): Option[Int] = {
     stream match {
       case _: TesslaCore.Nil | _: TesslaCore.InputStream => None
       case s: TesslaCore.Stream =>
+        if (memoized.contains(s.id)) return memoized(s.id)
 
         def visitChild(child: TesslaCore.StreamRef): Option[Int] =
           nestingDepth(streams, child, origin, memoized)
 
-        val result = if (s.name == origin) {
+        val result = if (s.id == origin) {
           Some(0)
         } else {
-          streams(s.name) match {
+          streams(s.id).expression match {
             case l: TesslaCore.Lift =>
               val foo: Seq[Option[Int]] = l.args.map(visitChild)
               foo.max
@@ -46,10 +46,14 @@ object RecursiveDepthChecker {
               visitChild(l.clock)
             case dl: TesslaCore.DelayedLast =>
               None
+            case m: TesslaCore.Merge =>
+              Seq(visitChild(m.stream1), visitChild(m.stream2)).max
+            case c: TesslaCore.Const =>
+              visitChild(c.stream)
           }
         }
 
-        memoized(s.name) = inc(result)
+        memoized(s.id) = inc(result)
         inc(result)
     }
   }
