5a51e86484cf08163c0e9e4da6dc2c1523c6fa3f
Hannes Kallwies <kallwies@isp.uni-luebeck.de>
2019-10-22 14:55:25 +0200

    Java template resource

---
+++ src/main/resources/de/uni_luebeck/isp/tessla/tessla_compiler/JavaSkeleton.java
@@ -0,0 +1,91 @@
+//This Java code was automatically created by tessla-compiler from a TeSSLa Specification
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static long currTs = 0;
+    private static long lastProcessedTs = -1;
+
+    private static void outputVar(String output, String trueName, long errorCode, long ts) {
+        String outputWithError = (errorCode != 0) ? "FATAL: " + trueName + " evaluation encountered an Error: " : "";
+        switch((int)errorCode) {
+            case 0:
+                outputWithError = output;
+                break;
+            case 4:
+                outputWithError += "Division by zero";
+                break;
+            case 8:
+                outputWithError += "Map access to non existing key";
+                break;
+            default:
+                outputWithError += "Unknown error code " + errorCode;
+        }
+        System.out.println(ts + ": " + outputWithError);
+        if (errorCode != 0) System.exit((int)errorCode);
+    }
+
+    public static void main(String[] args) {
+
+//VARDEF
+
+        try {
+            long newInputTs = 0;
+            String inputStream = "";
+            String value = "";
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String line;
+            boolean inputEndReached = false;
+
+            while (true) {
+
+                if ((line = br.readLine()) != null) {
+                    if (line.startsWith("$timeunit")) {
+                        System.out.println(line);
+                        continue;
+                    }
+                    newInputTs = java.lang.Long.parseLong(line.split(":")[0].trim());
+                    String rightPart = line.split(":")[1];
+                    inputStream = rightPart.split("=")[0].trim();
+                    if (rightPart.contains("=")) {
+                        value = rightPart.split("=")[1].trim();
+                    } else {
+                        value = "";
+                    }
+                } else {
+                    inputEndReached = true;
+                }
+
+                if (newInputTs > currTs || inputEndReached) {
+                    if (inputEndReached) {
+                        newInputTs++;
+                    }
+
+                    while (true) {
+
+//TRIGGER
+
+                        if (currTs == lastProcessedTs) break;
+
+//STEP
+
+                        lastProcessedTs = currTs;
+                    }
+
+                    currTs = newInputTs;
+                    if (inputEndReached) break;
+                } else if (newInputTs < currTs) {
+                    System.err.println(currTs + ": FATAL: decreasing timestamp received");
+                    System.exit(1);
+                }
+//INPUTPROCESSING
+            }
+        } catch (Exception e) {
+            System.err.println("ERROR parsing the input Stream\n\n" + e);
+            System.exit(2);
+        }
+    }
+}
