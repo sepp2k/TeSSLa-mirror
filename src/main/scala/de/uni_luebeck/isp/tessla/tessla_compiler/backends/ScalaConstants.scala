53b557ab09cf73f79c4681e7beafb4914ccd9c9f
Hannes Kallwies <kallwies@isp.uni-luebeck.de>
2020-02-04 19:07:16 +0100

    Include functions for immutable sets/maps/lists

--- src/main/scala/de/uni_luebeck/isp/tessla/tessla_compiler/backends/ScalaConstants.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/tessla_compiler/backends/ScalaConstants.scala
@@ -21,11 +21,11 @@ object ScalaConstants {
       case IntermediateCode.GeneralType => "Object"
       case IntermediateCode.OptionType(valType) => s"Option[${typeTranslation(valType, true)}]"
       case IntermediateCode.MutableSetType(valType) => s"scala.collection.mutable.HashSet[${typeTranslation(valType, true)}]"
-      case IntermediateCode.ImmutableSetType(valType) => s"scala.collection.immutable.HashSet[${typeTranslation(valType, true)}]"
+      case IntermediateCode.ImmutableSetType(valType) => s"Set[${typeTranslation(valType, true)}]"
       case IntermediateCode.MutableMapType(keyType, valType) => s"scala.collection.mutable.HashMap[${typeTranslation(keyType, true)}, ${typeTranslation(valType, true)}]"
-      case IntermediateCode.ImmutableMapType(keyType, valType) => s"scala.collection.immutable.HashMap[${typeTranslation(keyType, true)}, ${typeTranslation(valType, true)}]"
+      case IntermediateCode.ImmutableMapType(keyType, valType) => s"Map[${typeTranslation(keyType, true)}, ${typeTranslation(valType, true)}]"
       case IntermediateCode.MutableListType(valType) => s"scala.collection.mutable.ArrayBuffer[${typeTranslation(valType, true)}]"
-      case IntermediateCode.ImmutableListType(valType) => s"scala.collection.immutable.ArrayBuffer[${typeTranslation(valType, true)}]"
+      case IntermediateCode.ImmutableListType(valType) => s"List[${typeTranslation(valType, true)}]"
       case IntermediateCode.FunctionType(argsTypes, retType) => {
         val ret = ((if (argsTypes.size == 0) "" else ", ") + typeTranslation(retType))
         s"scala.Function${argsTypes.size}[${argsTypes.map(typeTranslation).mkString(", ")}${ret}]"
@@ -44,12 +44,12 @@ object ScalaConstants {
       case IntermediateCode.EmptyFunction(_) => "null"
       case IntermediateCode.None(_) => "None"
       case IntermediateCode.Some(content) => s"Option(${valueTranslation(content)})"
-      case IntermediateCode.EmptyMutableSet(valType) => s"new scala.collection.mutable.HashSet()"
-      case IntermediateCode.EmptyImmutableSet(valType) => s"new scala.collection.immutable.HashSet()"
-      case IntermediateCode.EmptyMutableMap(keyType, valType) => s"new scala.collection.mutable.HashMap()"
-      case IntermediateCode.EmptyImmutableMap(keyType, valType) => s"new scala.collection.immutable.HashMap()"
-      case IntermediateCode.EmptyMutableList(valType) => s"new scala.collection.mutable.ArrayBuffer()"
-      case IntermediateCode.EmptyImmutableList(valType) => s"new scala.collection.immutable.ArrayBuffer()"
+      case IntermediateCode.EmptyMutableSet(valType) => s"scala.collection.mutable.HashSet()"
+      case IntermediateCode.EmptyImmutableSet(valType) => s"Set()"
+      case IntermediateCode.EmptyMutableMap(keyType, valType) => s"scala.collection.mutable.HashMap()"
+      case IntermediateCode.EmptyImmutableMap(keyType, valType) => "Map()"
+      case IntermediateCode.EmptyMutableList(valType) => "scala.collection.mutable.ArrayBuffer()"
+      case IntermediateCode.EmptyImmutableList(valType) => "List()"
     }
   }
 
@@ -57,6 +57,7 @@ object ScalaConstants {
     name match {
       case "__[TC]output__" => s"outputVar(${getParseExpressionToString(typeHint.argsTypes(0), args(0))}, ${args(1)}, ${args(2)}, currTs)"
       case "__[TC]inputParse__" => getStringParseExpression(typeHint.retType, args(0))
+
       case "__ite__" => s"if (${args(0)}) ${args(1)} else ${args(2)}"
       case "__not__" => s"!${args(0)}"
       case "__negate__" |
@@ -88,11 +89,43 @@ object ScalaConstants {
       case "__bitxor__" => s"${args(0)} ^ ${args(1)}"
       case "__leftshift__" => s"${args(0)} << ${args(1)}"
       case "__rightshift__" => s"${args(0)} >> ${args(1)}"
+
       case "__Some__" => s"Option(${args(0)})"
       case "__getSome__" => s"${args(0)}.get"
       case "__isSome__" => s"${args(0)}.isDefined"
       case "__isNone__" => s"${args(0)}.isEmpty"
-      case _ => throw new Errors.CommandNotSupportedError(s"Unsupported built-in function for Java backend: $name")
+
+      //TODO: Handle mutable datastructures
+      case "__Map_empty__" => "Map()"
+      case "__Map_add__" => s"${args(0)} + (${args(1)} -> ${args(2)})"
+      case "__Map_contains__" => s"${args(0)}.contains(${args(1)})"
+      case "__Map_get__" => s"${args(0)}(${args(1)})"
+      case "__Map_remove__" => s"${args(0)} - ${args(1)}"
+      case "__Map_size__" => s"${args(0)}.size"
+      case "__Map_fold__" => s"${args(0)}.foldLeft[${typeTranslation(typeHint.argsTypes(1), true)}](${args(1)})(${args(2)})"
+      case "__Map_keys__" => s"${args(0)}.keys"
+
+      case "__Set_empty__" => "Set()"
+      case "__Set_add__" => s"${args(0)} :+ ${args(1)}"
+      case "__Set_contains__" => s"${args(0)}(${args(1)})"
+      case "__Set_remove__" => s"${args(0)} - ${args(1)}"
+      case "__Set_size__" => s"${args(0)}.size"
+      case "__Set_union__" => s"${args(0)}.union(${args(1)})"
+      case "__Set_intersection__" => s"${args(0)}.intersect(${args(1)})"
+      case "__Set_minus__" => s"${args(0)} -- ${args(1)}"
+      case "__Set_fold__" => s"${args(0)}.foldLeft[${typeTranslation(typeHint.argsTypes(1), true)}](${args(1)})(${args(2)})"
+
+      case "__List_empty__" => s"List()"
+      case "__List_size__" => s"${args(0)}.size"
+      case "__List_append__" => s"${args(0)} :+ ${args(1)}"
+      case "__List_prepend__" => s"${args(0)} +: ${args(1)}"
+      case "__List_tail__" => s"${args(0)}.tail"
+      case "__List_init__" => ???
+      case "__List_get__" => s"${args(0)}(${args(1)})"
+      case "__List_set__" => s"${args(0)}.updated(${args(1)}, ${args(2)})"
+      case "__List_fold__" => s"${args(0)}.foldLeft[${typeTranslation(typeHint.argsTypes(1), true)}](${args(1)})(${args(2)})"
+
+      case _ => throw Errors.CommandNotSupportedError(s"Unsupported built-in function for Java backend: $name")
     }
   }
 
