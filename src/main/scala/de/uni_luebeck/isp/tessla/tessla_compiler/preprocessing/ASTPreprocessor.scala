29695f3c336c0730a494fb9fddf0685d44cbe449
Hannes Kallwies <hannes.kallwies@student.uni-luebeck.de>
2020-03-23 23:10:04 +0100

    Deal params with name conflicts in ASTPreprocessor

--- src/main/scala/de/uni_luebeck/isp/tessla/tessla_compiler/preprocessing/ASTPreprocessor.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/tessla_compiler/preprocessing/ASTPreprocessor.scala
@@ -5,6 +5,7 @@ import de.uni_luebeck.isp.tessla.TesslaAST.Core
 import de.uni_luebeck.isp.tessla.TesslaAST.Core.{DefinitionExpression, _}
 import de.uni_luebeck.isp.tessla.{Location, TesslaAST, TranslationPhase}
 import de.uni_luebeck.isp.tessla.TranslationPhase.{Result, Success}
+import de.uni_luebeck.isp.tessla.tessla_compiler.mutability_check.ASTTransformation
 
 import scala.collection.mutable
 
@@ -17,7 +18,7 @@ class ASTPreprocessor(duplicateInlineCandidates: Boolean) extends TranslationPha
 
     var maxId: Long = spec.maxIdentifier
     val definitions = spec.definitions
-    val newIDs: mutable.Map[Identifier, Identifier] = mutable.Map()
+    val newIDs: mutable.Map[(Identifier, Option[Identifier]), Identifier] = mutable.Map()
     var warnings = Seq()
 
     def nextIDNum : Long = {
@@ -25,13 +26,13 @@ class ASTPreprocessor(duplicateInlineCandidates: Boolean) extends TranslationPha
       maxId
     }
 
-    def newIDFromOld(id: Identifier) : Identifier = {
+    def newIDFromOld(id: Identifier, f: Option[Identifier]) : Identifier = {
       val newID = if (id.idOrName.left.isDefined) {
         Identifier(Ior.both(id.idOrName.left.get, nextIDNum))
       } else {
         freshId()
       }
-      newIDs += (id -> newID)
+      newIDs += ((id, f) -> newID)
       newID
     }
 
@@ -39,100 +40,111 @@ class ASTPreprocessor(duplicateInlineCandidates: Boolean) extends TranslationPha
       Identifier(Ior.right(nextIDNum))
     }
 
-    def getActID(id: Identifier): Identifier = {
-      newIDs.getOrElse(id, id)
+    def getActID(id: Identifier, f: Option[Identifier]): Identifier = {
+      newIDs.getOrElse((id, f), id)
     }
 
-    def getExpRef(exp: ExpressionArg, add: mutable.Map[Identifier, DefinitionExpression], scope: Map[Identifier, DefinitionExpression]):  ExpressionArg = {
+    def getExpRef(exp: ExpressionArg, add: mutable.Map[Identifier, DefinitionExpression], scope: Map[Identifier, DefinitionExpression],
+                  fPars: Map[Identifier, Identifier]):  ExpressionArg = {
       exp match {
         case exp : Expression =>
           val newID = freshId()
-          add += (newID -> flattenDefExp(exp, add, scope))
+          add += (newID -> flattenDefExp(exp, add, scope, fPars, newID))
           ExpressionRef(newID, exp.tpe, Location.unknown)
-        case e: ExpressionRef if duplicateInlineCandidates && mutability_check.ASTTransformation.isInlineExpression(e, scope) =>
+        case e: ExpressionRef if duplicateInlineCandidates && ASTTransformation.isInlineExpression(e, scope) =>
           val newID = freshId()
-          val newExp = mutability_check.ASTTransformation.getInlineExpression(e, scope).asInstanceOf[DefinitionExpression]
-          add += (newID -> flattenDefExp(newExp, add, scope))
+          val newExp = ASTTransformation.getInlineExpression(e, scope).asInstanceOf[DefinitionExpression]
+          add += (newID -> flattenDefExp(newExp, add, scope, fPars, newID))
           ExpressionRef(newID, exp.tpe, Location.unknown)
         case e: ExpressionRef => e
       }
     }
 
-    def flattenDefExp(exp: DefinitionExpression, add: mutable.Map[Identifier, DefinitionExpression], scope: Map[Identifier, DefinitionExpression]) : DefinitionExpression = {
+    def flattenDefExp(exp: DefinitionExpression, add: mutable.Map[Identifier, DefinitionExpression], scope: Map[Identifier, DefinitionExpression],
+                      fPars: Map[Identifier, Identifier], assignID: Identifier) : DefinitionExpression = {
       exp match {
         case FunctionExpression(typeParams, params, body, result, location) => {
-          val newBody = mutable.Map.from(flattenDefs(body, scope ++ body))
-          val newResult = flattenExpArg(result, newBody, scope ++ body)
-          FunctionExpression(typeParams, params.map{case (id, s, t) => (newIDFromOld(id), s, t)}, newBody.toMap, newResult, location)
+          val newFPars = fPars ++ params.map{case (id, _, _) => (id, assignID)}
+          val newBody = mutable.Map.from(flattenDefs(body, scope ++ body, newFPars))
+          val newResult = getExpRef(result, newBody, scope ++ body, newFPars)
+          FunctionExpression(typeParams, params.map{case (id, s, t) => (newIDFromOld(id, Some(assignID)), s, t)}, newBody.toMap, newResult, location)
         }
         case ApplicationExpression(applicable, args, location) =>
           applicable match {
             case ExternExpression(_, _, _, "lift", _)
-                 | ExternExpression(_, _, _, "slift", _)
-                 | TypeApplicationExpression(ExternExpression(_, _, _, "lift", _), _, _)
-                 | TypeApplicationExpression(ExternExpression(_, _, _, "slift", _), _, _) =>
-              ApplicationExpression(applicable, args.dropRight(1).map(getExpRef(_, add, scope)) :+ args.last, location)
+               | ExternExpression(_, _, _, "slift", _)
+               | TypeApplicationExpression(ExternExpression(_, _, _, "lift", _), _, _)
+               | TypeApplicationExpression(ExternExpression(_, _, _, "slift", _), _, _) =>
+              ApplicationExpression(applicable, args.dropRight(1).map(getExpRef(_, add, scope, fPars)) :+ args.last, location)
             case _: ExternExpression |
                  TypeApplicationExpression(_: ExternExpression, _, _) =>
-              ApplicationExpression(applicable, args.map(getExpRef(_, add, scope)), location)
+              ApplicationExpression(applicable, args.map(getExpRef(_, add, scope, fPars)), location)
             case _ =>
-              ApplicationExpression(getExpRef(applicable, add, scope), args.map(getExpRef(_, add, scope)), location)
+              ApplicationExpression(getExpRef(applicable, add, scope, fPars), args.map(getExpRef(_, add, scope, fPars)), location)
           }
         case TypeApplicationExpression(applicable, typeArgs, location) =>
-          TypeApplicationExpression(flattenExpArg(applicable, add, scope), typeArgs, location)
+          TypeApplicationExpression(flattenExpArg(applicable, add, scope, fPars, assignID), typeArgs, location)
         case RecordConstructorExpression(entries, location) =>
-          val newEntries = entries.map{case (n, (e, l)) => (n, (getExpRef(e, add, scope), l))}
+          val newEntries = entries.map{case (n, (e, l)) => (n, (getExpRef(e, add, scope, fPars), l))}
           RecordConstructorExpression(newEntries, location)
         case RecordAccessorExpression(name, target, nameLocation, location) =>
-          RecordAccessorExpression(name, getExpRef(target, add, scope), nameLocation, location)
+          RecordAccessorExpression(name, getExpRef(target, add, scope, fPars), nameLocation, location)
         case _ => exp
       }
     }
 
-    def flattenExpArg(exp: ExpressionArg, add: mutable.Map[Identifier, DefinitionExpression], scope: Map[Identifier, DefinitionExpression]) : ExpressionArg = {
+    def flattenExpArg(exp: ExpressionArg, add: mutable.Map[Identifier, DefinitionExpression], scope: Map[Identifier, DefinitionExpression],
+                      fPars: Map[Identifier, Identifier], assignID: Identifier) : ExpressionArg = {
       exp match {
-        case exp: Expression => flattenDefExp(exp, add, scope)
-        case exp: ExpressionRef => getExpRef(exp, add, scope)
+        case exp: Expression => flattenDefExp(exp, add, scope, fPars, assignID)
+        case exp: ExpressionRef => getExpRef(exp, add, scope, fPars)
       }
     }
 
-    def flattenDefs(defs: Map[Identifier, DefinitionExpression], scope: Map[Identifier, DefinitionExpression]): Map[Identifier, DefinitionExpression] = {
+    def flattenDefs(defs: Map[Identifier, DefinitionExpression], scope: Map[Identifier, DefinitionExpression],
+                    fPars: Map[Identifier, Identifier]): Map[Identifier, DefinitionExpression] = {
       val newDefs : mutable.Map[Identifier, DefinitionExpression] = mutable.Map()
-      val transDefs = defs.map{case (id, defExp) => (newIDFromOld(id) -> flattenDefExp(defExp, newDefs, scope))}
+      val transDefs = defs.map{case (id, defExp) => val nid = newIDFromOld(id, None); (nid -> flattenDefExp(defExp, newDefs, scope, fPars, nid))}
       transDefs ++ newDefs.toMap
     }
 
-    def exchangeOldRefsDefExp(exp: DefinitionExpression) : DefinitionExpression = {
+    def exchangeOldRefsDefExp(exp: DefinitionExpression, fPars: Map[Identifier, Identifier]) : DefinitionExpression = {
+      def invNewIDs(id: Identifier) : (Identifier, Identifier) = {
+        val entry = newIDs.map(_.swap)(id)
+        (entry._1, entry._2.get)
+      }
+
       exp match {
         case FunctionExpression(typeParams, params, body, result, location) =>
-          FunctionExpression(typeParams, params, body.view.mapValues(exchangeOldRefsDefExp).toMap, exchangeOldRefsExpArg(result), location)
+          val newFPars = fPars ++ params.map{ case (id, _, _) => invNewIDs(id)}
+          FunctionExpression(typeParams, params, body.map{case (id, d) => (id -> exchangeOldRefsDefExp(d, newFPars))}, exchangeOldRefsExpArg(result, newFPars), location)
         case ApplicationExpression(applicable, args, location) =>
-          ApplicationExpression(exchangeOldRefsExpArg(applicable), args.map(exchangeOldRefsExpArg), location)
+          ApplicationExpression(exchangeOldRefsExpArg(applicable, fPars), args.map(exchangeOldRefsExpArg(_, fPars)), location)
         case TypeApplicationExpression(applicable, typeArgs, location) =>
-          TypeApplicationExpression(exchangeOldRefsExpArg(applicable), typeArgs, location)
+          TypeApplicationExpression(exchangeOldRefsExpArg(applicable, fPars), typeArgs, location)
         case RecordConstructorExpression(entries, location) =>
-          RecordConstructorExpression(entries.view.mapValues{case (e,l) => (exchangeOldRefsExpArg(e), l)}.toMap, location)
+          RecordConstructorExpression(entries.view.mapValues{case (e,l) => (exchangeOldRefsExpArg(e, fPars), l)}.toMap, location)
         case RecordAccessorExpression(name, target, nameLocation, location) =>
-          RecordAccessorExpression(name, exchangeOldRefsExpArg(target), nameLocation, location)
+          RecordAccessorExpression(name, exchangeOldRefsExpArg(target, fPars), nameLocation, location)
         case _ =>
           exp
       }
     }
 
-    def exchangeOldRefsExpArg(exp: ExpressionArg) : ExpressionArg = {
+    def exchangeOldRefsExpArg(exp: ExpressionArg, fPars: Map[Identifier, Identifier]) : ExpressionArg = {
       exp match {
-        case e: Expression => exchangeOldRefsDefExp(e)
-        case ExpressionRef(id, tpe, location) => ExpressionRef(getActID(id), tpe, location)
+        case e: Expression => exchangeOldRefsDefExp(e, fPars)
+        case ExpressionRef(id, tpe, location) => ExpressionRef(getActID(id, fPars.get(id)), tpe, location)
       }
     }
 
-    val newDefs = flattenDefs(definitions, definitions)
+    val newDefs = flattenDefs(definitions, definitions, Map())
 
-    Success(Core.Specification(spec.in.map{case (id, v) => (getActID(id), v)},
-      newDefs.view.mapValues(exchangeOldRefsDefExp).toMap,
-      spec.out.map{case (id, a) => (getActID(id), a)},
-      maxId),
-      warnings)
+    Success(Core.Specification(spec.in.map{case (id, v) => (getActID(id, None), v)},
+                               newDefs.map{case (id, d) => (id -> exchangeOldRefsDefExp(d, Map()))},
+                               spec.out.map{case (id, a) => (getActID(id, None), a)},
+                               maxId),
+            warnings)
   }
 
 }
