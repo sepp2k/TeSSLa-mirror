655a6ee2cdefe8e4dd5e31f2bd20184becec524d
Hannes Kallwies <hannes.kallwies@student.uni-luebeck.de>
2020-03-25 11:34:08 +0100

    Bugfix in No-Double-Write detection and printlns in comments
    Some Read-Before-Writes were detected which actually were not necessary

--- src/main/scala/de/uni_luebeck/isp/tessla/tessla_compiler/mutability_check/MutabilityChecker.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/tessla_compiler/mutability_check/MutabilityChecker.scala
@@ -42,6 +42,7 @@ object MutabilityChecker extends
     val expFlowAnalysis = new ExpressionFlowAnalysis(impCheck)
 
     def processDependencies(id: Identifier, dep: ExpressionFlowAnalysis.IdentifierDependencies) : Unit = {
+
       allMutableRelevantVars += id
 
       dep.calls.foreach(c => variableFamilies.union(c._1, c._2))
@@ -77,9 +78,9 @@ object MutabilityChecker extends
           passDependencies += (id -> dep.pass.map { d => if (passDependencies.contains(d)) passDependencies(d) else Set(d) }.foldLeft[Set[Identifier]](Set()) { case (c, s) => c ++ s })
         }
         cfDependencies(id).foreach(i => if (!nodes.contains(i)) nodes += i)
-
-        dep.pass.foreach{i => variableFamilies.union(id, i)}
       }
+
+      dep.pass.foreach{i => variableFamilies.union(id, i)}
     }
 
     def processDefinitions(defs: Seq[(Identifier, DefinitionExpression)], scope: Map[Identifier, DefinitionExpression]) : Unit = {
@@ -114,15 +115,25 @@ object MutabilityChecker extends
 
     //No Replicating Lasts
     def cleanParent(node: Identifier, beat: Seq[Identifier], caller: Identifier) : (Set[Identifier], Boolean) = {
-      if (writeMap.getOrElse(node, Set()).size > (if (beat.isEmpty) 1 else 0) || (beat.nonEmpty && !repsMap.getOrElse(node, Set()).filter(_._1 == beat.head).exists(c => impCheck.freqImplication(beat.head, c._2)))) {
+
+      //println(s"cleanParent: $node")
+
+      if (writeMap.getOrElse(node, Set()).size > (if (beat.isEmpty) 1 else 0) ||
+          (beat.nonEmpty && !repsMap.getOrElse(node, Set()).filter(_._1 == beat.head).exists(c => impCheck.freqImplication(beat.head, c._2)))) {
+        //println(s"cleanParent: $node : false")
         (Set(), false)
       } else {
         val (childReads, childClean) = dealChilds(node, beat, caller)
 
+        //println(s"cleanParent: $node --> childR/C $childReads $childClean")
+
         if (childClean) {
           val repParents = repsMap.flatMap{case (k, v) => if (v.exists(_._1 == node)) Some(k) else None}
-          repParents.map(cleanParent(_, node +: beat, node)).foldLeft[(Set[Identifier], Boolean)]((childReads, childClean)){case ((s1, b1), (s2, b2)) => (s1.union(s2), b1 && b2)}
+          val ret = repParents.map(cleanParent(_, node +: beat, node)).foldLeft[(Set[Identifier], Boolean)]((childReads, childClean)){case ((s1, b1), (s2, b2)) => (s1.union(s2), b1 && b2)}
+          //println(s"cleanParent: $node : $ret")
+          ret
         } else {
+          //println(s"cleanParent: $node : false2")
           (Set(), false)
         }
 
@@ -130,13 +141,22 @@ object MutabilityChecker extends
     }
 
     def cleanChild(node: Identifier, beat: Seq[Identifier], caller: Identifier) : (Set[Identifier], Boolean) = {
+
+      //println(s"cleanChild: $node")
+
       val cr : (Set[Identifier], Boolean) = if (beat.nonEmpty) cleanChild(node, beat.drop(1), caller) else (Set(), false)
-      if (cr._2) {
-        cr
-      } else if (beat.isEmpty || writeMap.getOrElse(node, Set()).nonEmpty || repsMap.getOrElse(caller, Set()).filter(_._1 == node).exists(c => !impCheck.freqImplication(beat.head, c._2))) {
+      val pr : (Set[Identifier], Boolean) = if (beat.isEmpty || writeMap.getOrElse(node, Set()).nonEmpty ||
+                 repsMap.getOrElse(caller, Set()).filter(_._1 == node).exists(c => !impCheck.freqImplication(beat.head, c._2))) {
         (Set(), false)
       } else {
         dealChilds(node, beat.drop(1), caller)
+      }
+      if (pr._2) {
+        //println(s"cleanChild: $node : $pr")
+        pr
+      } else {
+        //println(s"cleanChild: $node : $cr")
+        cr
       }
     }
 
@@ -144,26 +164,25 @@ object MutabilityChecker extends
       val childs : Set[Identifier] = repsMap.getOrElse(node, Set()).map(_._1) - caller
       if (childs.nonEmpty) {
         childs.map(c => cleanChild(c, beat, caller)).reduce[(Set[Identifier], Boolean)]{case ((s1, b1), (s2, b2)) => (s1.union(s2), b1 && b2)}
-      } else {
+      } else if (beat.isEmpty) {
         (readMap.getOrElse(node, Set()), true)
+      } else {
+        (Set(), true)
       }
     }
 
     val readBeforeWrites : collection.mutable.ArrayBuffer[(Identifier, Identifier, Identifier)] = collection.mutable.ArrayBuffer()
-    writeMap.filter(_._2.size == 1).foreach{case (writtenNode, writers) =>
-        writers.toSeq match {
-          case Seq(writeNode) =>
-            if (!immutVars.contains(writeNode)) {
-              val (readDeps, clean) = cleanParent(writtenNode, Seq(), writeNode)
-              if (!repsMap.getOrElse(writtenNode, Set()).map(_._1).contains(writeNode) && clean) {
-                  readDeps.foreach{r =>
-                    readBeforeWrites += ((r, writeNode, writtenNode))
-                  }
-              } else {
-                immutVars ++= variableFamilies.equivalenceClass(writtenNode)
+    writeMap.filter(_._2.size == 1).foreach{case (writtenNode, writeNodes) =>
+        val writeNode = writeNodes.toSeq.head
+        if (!immutVars.contains(writeNode)) {
+          val (readDeps, clean) = cleanParent(writtenNode, Seq(), writeNode)
+          if (!repsMap.getOrElse(writtenNode, Set()).map(_._1).contains(writeNode) && clean) {
+              readDeps.foreach{r =>
+                readBeforeWrites += ((r, writeNode, writtenNode))
               }
-            }
-          case _ =>
+          } else {
+            immutVars ++= variableFamilies.equivalenceClass(writtenNode)
+          }
         }
     }
 
@@ -176,7 +195,6 @@ object MutabilityChecker extends
     val addDeps = readBeforeWrites.flatMap{case (from, to, mut) =>
       if (!immutVars.contains(mut)) Some(from -> to) else None
     }.groupBy(_._1).view.mapValues(e => e.map(x => x._2).toSet).toMap
-
 
     /*
     println("========================")
@@ -191,8 +209,6 @@ object MutabilityChecker extends
     println(edges)
     println("-FAMILIES-")
     println(variableFamilies.toMap)
-    println("-IMMUT_VARS-")
-    println(immutVars)
     println("-FREQ_IMP-")
     println(impCheck.knownImplications)
     println("-Read_Before_Write-")
