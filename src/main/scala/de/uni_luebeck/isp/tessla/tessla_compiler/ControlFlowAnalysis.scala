6391199dcef64a84b47b9f0ac410d45f2cd79c4a
Hannes Kallwies <hannes.kallwies@student.uni-luebeck.de>
2020-04-15 13:48:43 +0200

    Control flow analysis
    for determination which streams do not have to be evaluated since they
    are only used inside of an if expression.
    
    Especially useful because writes to sets/maps etc. can be avoided this way.

--- src/main/scala/de/uni_luebeck/isp/tessla/tessla_compiler/ControlFlowAnalysis.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/tessla_compiler/ControlFlowAnalysis.scala
@@ -1,41 +1,96 @@
 package de.uni_luebeck.isp.tessla.tessla_compiler
 
+import de.uni_luebeck.isp.tessla.TesslaAST
 import de.uni_luebeck.isp.tessla.TesslaAST.Core._
+
+import scala.collection.mutable
 
 class ControlFlowAnalysis(spec: Specification) {
 
+  val usages: mutable.Map[TesslaAST.Core.Identifier, Set[TesslaAST.Core.Identifier]] = (new UsageMapCreation(spec)).getUsages
+
   val nonInline : collection.mutable.Set[Identifier] = collection.mutable.HashSet()
-  val inlinings : collection.mutable.HashMap[Identifier, Set[Identifier]] = collection.mutable.HashMap()
+  val conditions : collection.mutable.HashMap[Identifier, (Set[Identifier], Set[Identifier])] = collection.mutable.HashMap()
 
   def parseDefs(defs: Map[Identifier, DefinitionExpression], scope: Map[Identifier, DefinitionExpression]) : Unit = {
     val newScope = scope ++ defs
     defs.foreach{ case (id, expression) =>
-      parseExp(id, expression, None, newScope)
+      parseExp(id, expression, newScope)
     }
   }
 
-  def parseExp(currId: Identifier, ea: ExpressionArg, inlineTo: Option[Identifier], scope: Map[Identifier, DefinitionExpression]) : Unit = {
+  def isIfApplicable(app: ExpressionArg, args: Seq[ExpressionArg]) : Boolean = {
+    app match {
+      case TypeApplicationExpression(app, _, _) => isIfApplicable(app, args)
+      case ExternExpression(_, _, _, s, _) if Set("ite", "staticite").contains(s) => true
+      case ExternExpression(_, _, _, s, _) if Set("lift", "slift").contains(s) => isIfApplicable(args.last, args.dropRight(1))
+      case _ => false
+    }
+  }
+
+  def parseExp(currId: Identifier, ea: ExpressionArg, scope: Map[Identifier, DefinitionExpression]) : Unit = {
     ea match {
       case FunctionExpression(_, params, body, result, _) =>
         parseDefs(body, scope)
-        parseExp(currId, result, inlineTo, scope ++ body) //Dirty hack, we use currId, since we have no other
+        parseExp(currId, result, scope ++ body) //Dirty hack, we use currId, since we have no other
         nonInline ++= params.map(_._1)
       case ApplicationExpression(applicable, args, _) =>
-        applicable match {
-          case TypeApplicationExpression(ExternExpression(_, _, _, s, _), _, _) if Set("ite", "staticite", "and", "or").contains(s) =>
-            parseExp(currId, args(0), inlineTo, scope)
-            args.drop(1).foreach(parseExp(currId, _, Some(currId), scope))
-          case _ =>
-            parseExp(currId, applicable, inlineTo, scope)
-            args.foreach(parseExp(currId, _, inlineTo, scope))
+        if (isIfApplicable(applicable, args)) {
+            handleIf(args(0), args(1), args(2), scope)
         }
-      case TypeApplicationExpression(applicable, _, _) => parseExp(currId, applicable, inlineTo, scope)
-      case RecordAccessorExpression(_, target, _, _) => parseExp(currId, target, inlineTo, scope)
-      case RecordConstructorExpression(entries, _) => entries.foreach(e => parseExp(currId, e._2._1, inlineTo, scope))
-      case ExpressionRef(id, tpe, _) if !tpe.isInstanceOf[FunctionType] && inlineTo.isDefined && scope.contains(id) =>
-          inlinings += (id -> (inlinings.getOrElse(id, Set()) + inlineTo.get))
-          parseExp(currId, scope(id), inlineTo, scope)
+        parseExp(currId, applicable, scope)
+        args.foreach(parseExp(currId, _, scope))
+      case TypeApplicationExpression(applicable, _, _) => parseExp(currId, applicable, scope)
+      case RecordAccessorExpression(_, target, _, _) => parseExp(currId, target, scope)
+      case RecordConstructorExpression(entries, _) => entries.foreach(e => parseExp(currId, e._2._1, scope))
       case _ =>
+    }
+  }
+
+  def handleIf(cond: ExpressionArg, ic: ExpressionArg, ec: ExpressionArg, scope: Map[Identifier, DefinitionExpression]): Unit = {
+    cond match {
+      case r: ExpressionRef =>
+        addIfCond(r.id, false, ic, scope)
+        addIfCond(r.id, true, ec, scope)
+      case _ => //TODO: Error
+    }
+  }
+
+  def noLast(exp: ExpressionArg): Boolean = {
+    exp match {
+      case TypeApplicationExpression(applicable, _, _) => noLast(applicable)
+      case ApplicationExpression(ExternExpression(_, _, _, "last", _), _, _) => false
+      case _ => true
+    }
+  }
+
+  def addIfCond(cond: Identifier, neg: Boolean, exp: ExpressionArg, scope: Map[Identifier, DefinitionExpression]): Unit = {
+    if (!exp.tpe.isInstanceOf[FunctionType] && noLast(exp)) {
+      exp match {
+        case ApplicationExpression(_, args, _) => args.foreach {
+          addIfCond(cond, neg, _, scope)
+        }
+        case TypeApplicationExpression(applicable, _, _) => addIfCond(cond, neg, applicable, scope)
+        case RecordConstructorExpression(entries, _) => entries.foreach { case (_, (e, _)) => addIfCond(cond, neg, e, scope) }
+        case RecordAccessorExpression(_, target, _, _) => addIfCond(cond, neg, target, scope)
+        case ExpressionRef(id, _, _) => addIfCondId(cond, neg, id, scope)
+        case _ =>
+      }
+    }
+  }
+
+  def addIfCondId(cond: Identifier, neg: Boolean, id: Identifier, scope: Map[Identifier, DefinitionExpression]): Unit = {
+    if (usages(id).size == 1 && !spec.out.exists(_._1 == id)) {
+      val preCond : (Set[Identifier], Set[Identifier])= conditions.getOrElse(id, (Set(), Set()))
+      val newCond = if (neg) {
+        (preCond._1, preCond._2 + cond)
+      } else {
+        (preCond._1 + cond, preCond._2)
+      }
+      conditions += (id -> newCond)
+      if (scope.contains(id)) {
+        addIfCond(cond, neg, scope(id), scope)
+      }
     }
   }
 
@@ -49,7 +104,12 @@ class ControlFlowAnalysis(spec: Specification) {
     !nonInline.contains(id)
   }
 
-  def getInlineTo(id: Identifier) : Option[Set[Identifier]] = {
-    inlinings.get(id)
+  def getAddConditions(id: Identifier): (Set[Identifier], Set[Identifier]) = {
+    conditions.getOrElse(id, (Set(), Set()))
   }
+
+  def getAddOrderingConstraints: Set[(Identifier, Identifier)] = {
+    conditions.toSet.flatMap{case (id, (p, n)) => (p ++ n).map((_, id))}
+  }
+
 }
--- src/main/scala/de/uni_luebeck/isp/tessla/tessla_compiler/preprocessing/ASTRemoveUnused.scala
+++ src/main/scala/de/uni_luebeck/isp/tessla/tessla_compiler/preprocessing/ASTRemoveUnused.scala
@@ -9,31 +9,8 @@ class ASTRemoveUnused() extends TranslationPhase[Core.Specification, Core.Specif
 
   override def translate(spec: Core.Specification): Result[Core.Specification] = {
 
-    var usages : collection.mutable.HashMap[Identifier, Set[Identifier]] = collection.mutable.HashMap()
     var warnings = Seq()
-
-    def findUsages(ea: ExpressionArg, usedIn: Identifier): Unit = {
-      ea match {
-        case RecordAccessorExpression(_, target, _, _) => findUsages(target, usedIn)
-        case FunctionExpression(_, _, body, result, _) => findUsagesInDefs(body); findUsages(result, usedIn)
-        case ApplicationExpression(applicable, args, _) => (args :+ applicable).foreach(findUsages(_, usedIn))
-        case TypeApplicationExpression(applicable, _, _) => findUsages(applicable, usedIn)
-        case RecordConstructorExpression(entries, _) => entries.foreach(e => findUsages(e._2._1, usedIn))
-        case ExpressionRef(id, _, _) => usages += (id -> (usages.getOrElse(id, Set()) + usedIn))
-        case _ =>
-      }
-    }
-
-    def findUsagesInDefs(defs: Map[Identifier, DefinitionExpression]): Unit = {
-      defs.foreach{case (id, d) =>
-        if (!usages.contains(id)) {
-          usages += (id -> Set())
-        }
-        findUsages(d, id)
-      }
-    }
-
-    findUsagesInDefs(spec.definitions)
+    var usages = (new UsageMapCreation(spec)).getUsages
 
     usages --= spec.out.map(_._1.id)
     var dels : collection.mutable.HashSet[Identifier] = collection.mutable.HashSet()
