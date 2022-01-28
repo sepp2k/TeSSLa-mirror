48f5f6b22f39ff4f9bd66e6d40577406c0220429
Manuel Caldeira <manuel.caldeira@student.uni-luebeck.de>
2022-01-28 16:41:45 +0100

    Fix String parse impl

--- tessla-compiler/src/main/resources/de/uni_luebeck/isp/tessla/stdlib/rust/src/value.rs
+++ tessla-compiler/src/main/resources/de/uni_luebeck/isp/tessla/stdlib/rust/src/value.rs
@@ -422,16 +422,16 @@ impl TesslaDisplay for String {
     }
 }
 
-impl TesslaParse for TesslaString {
+impl TesslaParse for String {
     fn tessla_parse(string: &str) -> (Result<Self, &'static str>, &str) {
         let end = find_end(&string, "\"", 1);
         (if &string[0..1] == "\"" && &string[end..(end + 1)] == "\"" {
-            Ok(Value(string[1..end]
+            Ok(string[1..end]
                 .replace("\\\\n", "\n")
                 .replace("\\\\r", "\r")
                 .replace("\\\\t", "\t")
                 .replace("\\\\\"", "\"")
-                .replace("\\\\\\\\", "\\")))
+                .replace("\\\\\\\\", "\\"))
         } else {
             Err("Failed to parse String value")
         }, &string[(end + 1)..])
